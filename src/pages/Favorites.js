1e62a2f9f788219ac8494a19328c7b3863cd1096
Magda Sz <pesahson@gmail.com>
2022-03-17 16:50:11 +0100

    fix adding to favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -5,28 +5,31 @@ import axios from "axios";
 import Card from "../components/Card";
 
 function Favorites() {
-  const [favoriteMovies, setFavoriteMovies] = useState([]);
-  const movies = []
+  const [favoriteMovies, setFavoriteMovies] = useState([])
+  
+  //const movies = []
 
   useEffect(() => {
     const key = process.env.REACT_APP_API_KEY; 
 
+    // if (localStorage.getItem("movies") === null) {
+    //   localStorage.setItem("movies", "[]");
+    // }
 
-    if (localStorage.getItem("movies") === null) {
-      localStorage.setItem("movies", "[]");
-    }
-
-    const savedMovies = JSON.parse(localStorage.getItem("movies"));
+    const savedMovies = JSON.parse(localStorage.getItem("movies")) || [];
    
     for (let i = 0; i < savedMovies.length; i++) {
       axios.get(
         `https://api.themoviedb.org/3/movie/${savedMovies[i]}?api_key=${key}&language=en-US`
       )
       .then(res => {
-        movies.push(res.data)
-      setFavoriteMovies([...movies])});
-     
-       }
+        //movies.push(res.data)
+        setFavoriteMovies((prev) => [...prev, res.data])
+        
+      //setFavoriteMovies([...movies])
+    });
+    }
+   
       }, []);
       
       
@@ -36,7 +39,7 @@ function Favorites() {
       <main>
         <div className="favorites-container">
           {favoriteMovies.map(movie => {
-            return <Card movie={movie}/>
+            return <Card key={movie.id} movie={movie}/>
           })}
         </div>
       </main>
